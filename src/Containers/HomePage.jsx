7df16527ee776f04d1d3973b17799a2f57a0f388
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-03-26 16:12:32 -0400

    Updated Home page with dynamic clock/date
    Added React-Moment to package, utilized Moment component

--- src/Containers/HomePage.jsx
+++ src/Containers/HomePage.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 const HomePage = props => {
 
+
+
+
   return (
     <div>
-      <h1 className="home-clock">12:30 PM</h1>
+      <h1 className="home-time"><Moment format="h:mm A" interval={1000}/></h1>
+      <h3 className="home-date"><Moment format="dddd, MMMM Do YYYY" interval={60000}/></h3>
       <h2 className="home-weather">58° Partly Cloudy</h2>
-      <p className="home-quote">This is a placeholder motivational quote, meant to make you feel good." - Some Wise Person</p>
+      <p className="home-quote">This is a placeholder motivational quote, can't you tell?" - Some Wise Person</p>
     </div>
   )
 
