9d634b592a92672f178b9cecab1c7ec8397a1120
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-04-07 17:00:28 -0400

    added ability to update User profile picture
    Added an additional modal with a text input for profile pic URL
    
    Added new fetch to patch User instance in backend

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
+
+import EditModal from '../Components/EditModal';
+
+import { updateProfilePic } from '../fetches';
 
 import profile_pic_placeholder from './profile_pic_placeholder.png'
 
 const Profile = props => {
 
+  // STATE
+  const [showEdit, setShowEdit] = useState(false)
+  const [showEditModal, setShowEditModal] = useState(false)
+  const [userState, setUserState] = useState("")
+
   // REDUX
   const user = useSelector( state => state.user )
   const { username, profile_pic, time_meditated, time_focused } = user
+
+  useEffect( () => {
+    setUserState(user)
+  }, [user])
+
+
+
 
   
   // returns a formatted version of the total time meditated in JSX, converted from raw seconds
@@ -43,19 +59,50 @@ const Profile = props => {
       return <p><strong>{seconds}</strong> <em>{seconds === 1 ? "second" : "seconds"}</em></p>
     }
   }
-  
+
+
+
+
+  // sends off a PATCH request to the backend, updating a User's saved profile pic URL
+  const newProfilePic = url => {
+    updateProfilePic(user.id, url)
+    setUserState({
+      ...userState,
+      profile_pic: url
+    })
+    setShowEditModal(false)
+  }
+
+
+
 
   
   // RENDER
   return (
     <div className="profile-container">
+
+      <EditModal 
+        show={showEditModal}
+        onHide={() => setShowEditModal(false)}
+        profilePic={profile_pic}
+        update={newProfilePic}
+      />
+
       <div className="profile-content">
+
         <h1>Hello, {username}</h1>
-        {user.profile_pic ? <img className="profile-pic" src={profile_pic} alt="profile pic" /> : <img className="profile-pic" src={profile_pic_placeholder} alt="profile pic" />}
+
+        <div className="profile-pic-container" onMouseEnter={() => setShowEdit(true)} onMouseLeave={() => setShowEdit(false)}>
+          {userState.profile_pic ? <img className="profile-pic" src={userState.profile_pic} alt="profile pic" /> : <img className="profile-pic" src={profile_pic_placeholder} alt="profile pic" />}
+          {showEdit ? <p className="edit-profile-pic" onClick={() => setShowEditModal(true)}>edit profile picture</p> : null }
+        </div>
+
         <h3>total time meditated:</h3>
         {returnTimeMeditated()}
+
         <h3>total time focused:</h3>
         {returnTimeFocused()}
+
       </div>
     </div>
   )
